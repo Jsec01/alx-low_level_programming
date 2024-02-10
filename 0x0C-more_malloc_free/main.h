c77174194b45057b721aa7d7a4a61e7f662befcb
Jsec01 <lehlohonolomaloka001@gmail.com>
2024-02-10 09:36:37 -0800

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _print(char *str, int l);
+char *mul(char n, char *num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat( char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmomb, unsigned int size);
+int *array_range(int main, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
